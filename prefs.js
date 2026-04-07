2bd375feeda76f34f3c39f41a3468e278dfb3e7e
dexterhere04 <sdrtharun@gmail.com>
2026-04-07 23:40:44 +0530

    feat(prefs.js): migrate to Adw UI and ExtensionPreferences
    - Replace Gtk.Grid with Adw.PreferencesPage
    - Use Adw.PreferencesGroup and Adw.ActionRow
    - Migrate from buildPrefsWidget to ExtensionPreferences.fillPreferencesWindow
    - Use Gtk.FileChooserNative for file selection
    - Auto-apply wallpaper preview when changing settings

--- prefs.js
+++ prefs.js
@@ -2,210 +2,199 @@
 
 'use strict';
 
-const { Gio, GObject, Gtk } = imports.gi;
+import Adw from 'gi://Adw';
+import GObject from 'gi://GObject';
+import Gtk from 'gi://Gtk';
+import Gio from 'gi://Gio';
+import { ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const SettingsUi = Me.imports.settingsUi;
-const Utils = Me.imports.utils;
+import SettingsUi from './settingsUi.js';
+import { SwitchTime, getAvailablePictureOptions, isDayNow } from './utils.js';
 
-let DayNightWallpaperPrefsWidget = GObject.registerClass(
-class DayNightWallpaperPrefsWidget extends Gtk.Grid {
-    _init(settings) {
-        super._init({
-            margin: 18,
-            column_spacing: 12,
-            row_spacing: 12,
-        })
+const SCHEMA_ID = 'org.gnome.shell.extensions.day-night-wallpaper';
 
-        // Day-Night Wallpaper settings
-        this._settings = settings;
-
-        // Image adjustment options
-        this._pictureOptions = Utils.getAvailablePictureOptions().sort();
-
-        // Image files filter
-        this._imageFileFilter = new Gtk.FileFilter();
-        this._imageFileFilter.set_name('Image files');
-        this._imageFileFilter.add_mime_type('image/jpg');
-        this._imageFileFilter.add_mime_type('image/png');
-        this._imageFileFilter.add_mime_type('image/jpeg');
-        this._imageFileFilter.add_mime_type('image/bmp');
-
-        this._dayWallpaperImageChooserButton = this._createWallpaperImageChooserButton(settings.get_string('day-wallpaper'));
-        this._dayWallpaperAdjustmentComboBoxText = this._createWallpaperAdjustmentComboBoxText(settings.get_string('day-wallpaper-adjustment'));
-        this._dayWallpaperSwitchTimeWidget = new SettingsUi.SwitchTimeWidget();
-        const _dayWallpaperSwitchTime = Utils.SwitchTime.newFromSettings(settings.get_double('day-wallpaper-switch-time'));
-        this._dayWallpaperSwitchTimeWidget.setSwitchTime(_dayWallpaperSwitchTime.switchHour, _dayWallpaperSwitchTime.switchMinute);
-
-        this._nightWallpaperImageChooserButton = this._createWallpaperImageChooserButton(settings.get_string('night-wallpaper'));
-        this._nightWallpaperAdjustmentComboBoxText = this._createWallpaperAdjustmentComboBoxText(settings.get_string('night-wallpaper-adjustment'));
-        this._nightWallpaperSwitchTimeWidget = new SettingsUi.SwitchTimeWidget();
-        const _nightWallpaperSwitchTime = Utils.SwitchTime.newFromSettings(settings.get_double('night-wallpaper-switch-time'));
-        this._nightWallpaperSwitchTimeWidget.setSwitchTime(_nightWallpaperSwitchTime.switchHour, _nightWallpaperSwitchTime.switchMinute);
-
-        this._buildDayWallpaperSection();
-        this._buildNightWallpaperSection();
-        this._buildAboutSection();
-
-        this._connectSettings();
-    }
-
-    _createWallpaperImageChooserButton(imageUri) {
-        let wallpaperImageChooserButton = new Gtk.FileChooserButton({
-            title: 'Image File',
-            action: Gtk.FileChooserAction.OPEN,
-            halign: Gtk.Align.END,
-            width_chars: 40,
-            filter: this._imageFileFilter
-        });
-        wallpaperImageChooserButton.set_uri(imageUri);
-        return wallpaperImageChooserButton;
-    }
-
-    _createWallpaperAdjustmentComboBoxText(adjustmentOption) {
-        let wallpaperAdjustmentComboBoxText = new Gtk.ComboBoxText();
-        let adjustmentOptionIndex = 0;
-        for (let i = 0; i < this._pictureOptions.length; i++) {
-            if (this._pictureOptions[i] == adjustmentOption) {
-                adjustmentOptionIndex = i;
-            }
-            wallpaperAdjustmentComboBoxText.append_text(this._capitalise(this._pictureOptions[i]));
-        }
-        wallpaperAdjustmentComboBoxText.set_active(adjustmentOptionIndex);
-        return wallpaperAdjustmentComboBoxText;
-    }
-
-    _buildDayWallpaperSection() {
-        // Day wallpaper title
-        const _dayWallpaperTitleLabel = new Gtk.Label({
-            label: `<b>Day Wallpaper</b>`,
-            halign: Gtk.Align.START,
-            use_markup: true
-        });
-        this.attach(_dayWallpaperTitleLabel, 0, 1, 2, 1);
-
-        // Day wallpaper image
-        const _dayWallpaperImageLabel = new Gtk.Label({
-            label: 'Image',
-            halign: Gtk.Align.START,
-            hexpand: true
-        });
-        this.attach(_dayWallpaperImageLabel, 0, 2, 1, 1);
-        this.attach_next_to(this._dayWallpaperImageChooserButton, _dayWallpaperImageLabel, Gtk.PositionType.RIGHT, 1, 1);
-
-        // Day wallpaper adjustment
-        const _dayWallpaperAdjustmentLabel = new Gtk.Label({
-            label: 'Adjustment',
-            halign: Gtk.Align.START,
-            hexpand: true
-        });
-        this.attach(_dayWallpaperAdjustmentLabel, 0, 3, 1, 1);
-        this.attach_next_to(this._dayWallpaperAdjustmentComboBoxText, _dayWallpaperAdjustmentLabel, Gtk.PositionType.RIGHT, 1, 1);
-
-        // Day wallpaper switch time
-        const _dayWallpaperSwitchTimeLabel = new Gtk.Label({
-            label: 'Switch Time (24-hour)',
-            halign: Gtk.Align.START,
-            hexpand: true
-        });
-        this.attach(_dayWallpaperSwitchTimeLabel, 0, 4, 1, 1);
-        this.attach_next_to(this._dayWallpaperSwitchTimeWidget, _dayWallpaperSwitchTimeLabel, Gtk.PositionType.RIGHT, 1, 1);
-    }
-
-    _buildNightWallpaperSection() {
-        // Night wallpaper title
-        const _nightWallpaperTitleLabel = new Gtk.Label({
-            label: `<b>Night Wallpaper</b>`,
-            halign: Gtk.Align.START,
-            use_markup: true,
-            margin_top: 18
-        });
-        this.attach(_nightWallpaperTitleLabel, 0, 5, 2, 1)
-
-        // Night wallpaper image
-        const _nightWallpaperImageLabel = new Gtk.Label({
-            label: 'Image',
-            halign: Gtk.Align.START,
-            hexpand: true
-        });
-        this.attach(_nightWallpaperImageLabel, 0, 6, 1, 1);
-        this.attach_next_to(this._nightWallpaperImageChooserButton, _nightWallpaperImageLabel, Gtk.PositionType.RIGHT, 1, 1);
-
-        // Night wallpaper adjustment
-        const _nightWallpaperAdjustmentLabel = new Gtk.Label({
-            label: 'Adjustment',
-            halign: Gtk.Align.START,
-            hexpand: true
-        });
-        this.attach(_nightWallpaperAdjustmentLabel, 0, 7, 1, 1);
-        this.attach_next_to(this._nightWallpaperAdjustmentComboBoxText, _nightWallpaperAdjustmentLabel, Gtk.PositionType.RIGHT, 1, 1);
-
-        // Night wallpaper switch time
-        const _nightWallpaperSwitchTimeLabel = new Gtk.Label({
-            label: 'Switch Time (24-hour)',
-            halign: Gtk.Align.START,
-            hexpand: true
-        });
-        this.attach(_nightWallpaperSwitchTimeLabel, 0, 8, 1, 1);
-        this.attach_next_to(this._nightWallpaperSwitchTimeWidget, _nightWallpaperSwitchTimeLabel, Gtk.PositionType.RIGHT, 1, 1);
-    }
-
-    _buildAboutSection() {
-        const _aboutSection = new SettingsUi.AboutSection();
-        _aboutSection.set_margin_top(18);
-        this.attach(_aboutSection, 0, 9, 2, 1);
-    }
-
-    _onDayWallpaperSwitchTimeChanged(spinButton) {
-        const daySwitchHour = this._dayWallpaperSwitchTimeWidget.hourSpinButton.get_value_as_int();
-        const daySwitchMinute = this._dayWallpaperSwitchTimeWidget.minuteSpinButton.get_value_as_int();
-        const daySwitchTime = new Utils.SwitchTime(daySwitchHour, daySwitchMinute);
-        this._settings.set_double('day-wallpaper-switch-time', daySwitchTime.toSettingsFormat());
-    }
-
-    _onNightWallpaperSwitchTimeChanged(spinButton) {
-        const nightSwitchHour = this._nightWallpaperSwitchTimeWidget.hourSpinButton.get_value_as_int();
-        const nightSwitchMinute = this._nightWallpaperSwitchTimeWidget.minuteSpinButton.get_value_as_int();
-        const nightSwitchTime = new Utils.SwitchTime(nightSwitchHour, nightSwitchMinute);
-        this._settings.set_double('night-wallpaper-switch-time', nightSwitchTime.toSettingsFormat());
-    }
-
-    _connectSettings() {
-        this._dayWallpaperImageChooserButton.connect('file-set', () => {
-            this._settings.set_string('day-wallpaper', this._dayWallpaperImageChooserButton.get_uri());
-        });
-        this._dayWallpaperAdjustmentComboBoxText.connect('changed', () => {
-            const activeItem = this._dayWallpaperAdjustmentComboBoxText.get_active();
-            this._settings.set_string('day-wallpaper-adjustment', this._pictureOptions[activeItem]);
-        });
-        this._dayWallpaperSwitchTimeWidget.hourSpinButton.connect('value-changed', this._onDayWallpaperSwitchTimeChanged.bind(this));
-        this._dayWallpaperSwitchTimeWidget.minuteSpinButton.connect('value-changed', this._onDayWallpaperSwitchTimeChanged.bind(this));
-
-        this._nightWallpaperImageChooserButton.connect('file-set', () => {
-            this._settings.set_string('night-wallpaper', this._nightWallpaperImageChooserButton.get_uri());
-        });
-        this._nightWallpaperAdjustmentComboBoxText.connect('changed', () => {
-            const activeItem = this._nightWallpaperAdjustmentComboBoxText.get_active();
-            this._settings.set_string('night-wallpaper-adjustment', this._pictureOptions[activeItem]);
-        });
-        this._nightWallpaperSwitchTimeWidget.hourSpinButton.connect('value-changed', this._onNightWallpaperSwitchTimeChanged.bind(this));
-        this._nightWallpaperSwitchTimeWidget.minuteSpinButton.connect('value-changed', this._onNightWallpaperSwitchTimeChanged.bind(this));
-    }
-
-    _capitalise(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1)
-    }
-});
-
-function init() {
-    Utils.checkExtensionSettings();
+function getSettings() {
+    return Gio.Settings.new(SCHEMA_ID);
 }
 
-function buildPrefsWidget() {
-    const settings = ExtensionUtils.getSettings();
-    const prefsWidget = new DayNightWallpaperPrefsWidget(settings);
-    prefsWidget.show_all();
+export default class DayNightWallpaperPreferences extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+        const settings = getSettings();
+        const pictureOptions = getAvailablePictureOptions().sort();
 
-    return prefsWidget;
+        const imageFileFilter = new Gtk.FileFilter();
+        imageFileFilter.set_name('Image files');
+        imageFileFilter.add_mime_type('image/jpg');
+        imageFileFilter.add_mime_type('image/png');
+        imageFileFilter.add_mime_type('image/jpeg');
+        imageFileFilter.add_mime_type('image/bmp');
+
+        const page = new Adw.PreferencesPage();
+        
+        const dayGroup = new Adw.PreferencesGroup();
+        dayGroup.set_title('Day Wallpaper');
+        page.add(dayGroup);
+
+        const dayImageRow = new Adw.ActionRow({ title: 'Image' });
+        const dayImageButton = new Gtk.Button({ label: 'Choose...' });
+        dayImageRow.add_suffix(dayImageButton);
+        dayImageRow.set_activatable_widget(dayImageButton);
+        dayGroup.add(dayImageRow);
+
+        const dayAdjRow = new Adw.ActionRow({ title: 'Adjustment' });
+        const dayAdjustmentCombo = new Gtk.ComboBoxText();
+        pictureOptions.forEach(opt => dayAdjustmentCombo.append_text(opt.charAt(0).toUpperCase() + opt.slice(1)));
+        dayAdjustmentCombo.set_active(0);
+        dayAdjRow.add_suffix(dayAdjustmentCombo);
+        dayAdjRow.set_activatable_widget(dayAdjustmentCombo);
+        dayGroup.add(dayAdjRow);
+
+        const dayTimeRow = new Adw.ActionRow({ title: 'Switch Time (24h)' });
+        const dayTimeWidget = new SettingsUi.SwitchTimeWidget();
+        dayTimeRow.add_suffix(dayTimeWidget);
+        dayTimeRow.set_activatable_widget(dayTimeWidget.hourSpinButton);
+        dayGroup.add(dayTimeRow);
+
+        const nightGroup = new Adw.PreferencesGroup();
+        nightGroup.set_title('Night Wallpaper');
+        page.add(nightGroup);
+
+        const nightImageRow = new Adw.ActionRow({ title: 'Image' });
+        const nightImageButton = new Gtk.Button({ label: 'Choose...' });
+        nightImageRow.add_suffix(nightImageButton);
+        nightImageRow.set_activatable_widget(nightImageButton);
+        nightGroup.add(nightImageRow);
+
+        const nightAdjRow = new Adw.ActionRow({ title: 'Adjustment' });
+        const nightAdjustmentCombo = new Gtk.ComboBoxText();
+        pictureOptions.forEach(opt => nightAdjustmentCombo.append_text(opt.charAt(0).toUpperCase() + opt.slice(1)));
+        nightAdjustmentCombo.set_active(0);
+        nightAdjRow.add_suffix(nightAdjustmentCombo);
+        nightAdjRow.set_activatable_widget(nightAdjustmentCombo);
+        nightGroup.add(nightAdjRow);
+
+        const nightTimeRow = new Adw.ActionRow({ title: 'Switch Time (24h)' });
+        const nightTimeWidget = new SettingsUi.SwitchTimeWidget();
+        nightTimeRow.add_suffix(nightTimeWidget);
+        nightTimeRow.set_activatable_widget(nightTimeWidget.hourSpinButton);
+        nightGroup.add(nightTimeRow);
+
+        window.add(page);
+
+        const daySwitchTime = SwitchTime.newFromSettings(settings.get_double('day-wallpaper-switch-time'));
+        dayTimeWidget.setSwitchTime(daySwitchTime.switchHour, daySwitchTime.switchMinute);
+
+        const nightSwitchTime = SwitchTime.newFromSettings(settings.get_double('night-wallpaper-switch-time'));
+        nightTimeWidget.setSwitchTime(nightSwitchTime.switchHour, nightSwitchTime.switchMinute);
+
+        const currentDayWallpaper = settings.get_string('day-wallpaper');
+        if (currentDayWallpaper) {
+            try {
+                const file = Gio.File.new_for_uri(currentDayWallpaper);
+                dayImageButton.set_label(file.get_basename().substring(0, 20) || 'Choose...');
+            } catch (e) {}
+        }
+
+        const currentNightWallpaper = settings.get_string('night-wallpaper');
+        if (currentNightWallpaper) {
+            try {
+                const file = Gio.File.new_for_uri(currentNightWallpaper);
+                nightImageButton.set_label(file.get_basename().substring(0, 20) || 'Choose...');
+            } catch (e) {}
+        }
+
+        dayImageButton.connect('clicked', () => {
+            const file = Gtk.FileChooserNative.new(
+                'Select Day Wallpaper',
+                null,
+                Gtk.FileChooserAction.OPEN,
+                '_Open',
+                '_Cancel'
+            );
+            
+            file.connect('response', (dlg, response) => {
+                if (response === Gtk.ResponseType.ACCEPT) {
+                    const selectedFile = dlg.get_file();
+                    if (selectedFile) {
+                        const uri = selectedFile.get_uri();
+                        settings.set_string('day-wallpaper', uri);
+                        dayImageButton.set_label(selectedFile.get_basename().substring(0, 20));
+                        
+                        const daySwitchTime = settings.get_double('day-wallpaper-switch-time');
+                        const nightSwitchTime = settings.get_double('night-wallpaper-switch-time');
+                        const isCurrentlyDay = isDayNow(daySwitchTime, nightSwitchTime);
+                        
+                        if (isCurrentlyDay) {
+                            const bg = Gio.Settings.new('org.gnome.desktop.background');
+                            bg.set_string('picture-uri', uri);
+                            bg.set_string('picture-uri-dark', uri);
+                        }
+                    }
+                }
+                dlg.destroy();
+            });
+            file.show();
+        });
+
+        nightImageButton.connect('clicked', () => {
+            const file = Gtk.FileChooserNative.new(
+                'Select Night Wallpaper',
+                null,
+                Gtk.FileChooserAction.OPEN,
+                '_Open',
+                '_Cancel'
+            );
+            
+            file.connect('response', (dlg, response) => {
+                if (response === Gtk.ResponseType.ACCEPT) {
+                    const selectedFile = dlg.get_file();
+                    if (selectedFile) {
+                        const uri = selectedFile.get_uri();
+                        settings.set_string('night-wallpaper', uri);
+                        nightImageButton.set_label(selectedFile.get_basename().substring(0, 20));
+                        
+                        const daySwitchTime = settings.get_double('day-wallpaper-switch-time');
+                        const nightSwitchTime = settings.get_double('night-wallpaper-switch-time');
+                        const isCurrentlyDay = isDayNow(daySwitchTime, nightSwitchTime);
+                        
+                        if (!isCurrentlyDay) {
+                            const bg = Gio.Settings.new('org.gnome.desktop.background');
+                            bg.set_string('picture-uri', uri);
+                            bg.set_string('picture-uri-dark', uri);
+                        }
+                    }
+                }
+                dlg.destroy();
+            });
+            file.show();
+        });
+
+        dayTimeWidget.hourSpinButton.connect('value-changed', () => {
+            const hour = dayTimeWidget.hourSpinButton.get_value_as_int();
+            const minute = dayTimeWidget.minuteSpinButton.get_value_as_int();
+            const time = new SwitchTime(hour, minute);
+            settings.set_double('day-wallpaper-switch-time', time.toSettingsFormat());
+        });
+
+        dayTimeWidget.minuteSpinButton.connect('value-changed', () => {
+            const hour = dayTimeWidget.hourSpinButton.get_value_as_int();
+            const minute = dayTimeWidget.minuteSpinButton.get_value_as_int();
+            const time = new SwitchTime(hour, minute);
+            settings.set_double('day-wallpaper-switch-time', time.toSettingsFormat());
+        });
+
+        nightTimeWidget.hourSpinButton.connect('value-changed', () => {
+            const hour = nightTimeWidget.hourSpinButton.get_value_as_int();
+            const minute = nightTimeWidget.minuteSpinButton.get_value_as_int();
+            const time = new SwitchTime(hour, minute);
+            settings.set_double('night-wallpaper-switch-time', time.toSettingsFormat());
+        });
+
+        nightTimeWidget.minuteSpinButton.connect('value-changed', () => {
+            const hour = nightTimeWidget.hourSpinButton.get_value_as_int();
+            const minute = nightTimeWidget.minuteSpinButton.get_value_as_int();
+            const time = new SwitchTime(hour, minute);
+            settings.set_double('night-wallpaper-switch-time', time.toSettingsFormat());
+        });
+    }
 }
